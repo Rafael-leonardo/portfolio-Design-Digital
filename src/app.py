4d448b6c16d52513071312bca83100b12377b0f8
Rafael <rafaelleolopes123@gmail.com>
2021-11-14 22:39:47 -0300

    adicionando responsabilidade ao site utilizando bootstrap

--- src/app.py
+++ src/app.py
@@ -1,5 +1,24 @@
-from app import db, app
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/curriculo")
+def curriculo():
+    return render_template("curriculo.html")
+
+@app.route("/projetos")
+def projetos():
+    return render_template("projetos.html")
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html")
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(port=8000, debug=True)
